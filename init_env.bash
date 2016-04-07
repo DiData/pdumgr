d3fe05f7dc42c0983264a3b418abfbe28323166f
Jeremy Sayres <jeremy.sayres@itaas.dimensiondata.com>
2016-04-07 18:43:30 +0000

    init updated

--- init_env.bash
+++ init_env.bash
@@ -3,7 +3,7 @@
 if [ ! -d "env" ]; then
     virtualenv env
     source env/bin/activate
-    pip install pysnmp regex python-nmap
+    pip install pysnmp regex python-nmap requests
 else
     source env/bin/activate
 fi
