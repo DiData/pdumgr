c0df1be5c48a1efdb2f02dbb4a40a27880548527
Jeremy Sayres <jeremy.sayres@itaas.dimensiondata.com>
2016-04-08 02:02:48 +0000

    reorg pdumgr file

--- pdumgr.py
+++ pdumgr.py
@@ -1,104 +1,76 @@
 #!/usr/bin/env python
-
-from requests import get as reqget, post as reqpost
-from json import loads as jsonloads, dumps as jsondumps
-from snmp_helper import snmp_get_oid,snmp_extract
-from regex import search as regsearch
-from nmap import PortScanner
-from config import configSystem
+import os
+from hashlib import sha1
 from threading import Thread
+from ftplib import FTP
+import time
 
-import os, sys, getopt, pwd
+# The purpose of this script is very simple. We loop through a list of IPv6 auto configure addresses:
+# - pull the config down via FTP
+# - check for IPv4=disabled
+# - replace with IPv4=enabled
+# - upload config back up to FTP
 
-cfg = configSystem('config.cfg')
+# IPv6 Interface (only auto configuration required.)
+interface = 'eth1'
+# Default APC PDU username
+defaultUser = 'apc'
+# Default APC PDU password
+defaultPass = 'apc'
+# Put your IPv6 addresses in here (delimited by new line \n)
+file = 'iplist.txt'
 
-def probePdu(host=None, state=None, snmp_port=161, ro_community=None, rw_community=None, dccode=None, url=None):
-    print 'host: %s state: %s' % (host, state)
-    if state == 'closed':
-        return False
-
-    pdu = (host, ro_community, snmp_port)
-    snmp_data = snmp_get_oid(pdu, oid='.1.3.6.1.2.1.1.1.0', display_errors=False)
-    if snmp_data is None:
-        return False
-
-    output = snmp_extract(snmp_data)
-    if output is None:
-        return False
-
-    data = {'ip_address': host, 'dccode': dccode}
-
-    m = regsearch(r'SN.+([0-9A-Z]{12})', output)
-    if m:
-        data['real_serial'] = m.group(1)
-
-    m = regsearch(r'MN:([0-9A-Z]+)', output)
-    if m:
-        data['real_model'] = m.group(1)
-
-    m = regsearch(r'MB:([v0-9\.]+)', output)
-    if m:
-        data['mbver'] = m.group(1)
-
-    m = regsearch(r'HR:([A-Z0-9]+)', output)
-    if m:
-        data['hwrev'] = m.group(1)
-
-    m = regsearch(r'PF:([v0-9\.]+)', output)
-    if m:
-        data['apcver'] = m.group(1)
-
-    m = regsearch(r'PN:(.*?)\.bin', output)
-    if m:
-        data['apcverfile'] = m.group(1)
-
-    m = regsearch(r'AF1:([v0-9\.]+)', output)
-    if m:
-        data['appmodver'] = m.group(1)
-
-    m = regsearch(r'AN1:(.*?)\.bin', output)
-    if m:
-        data['appmodverfile'] = m.group(1)
-
-    r = reqpost(url, headers={'SB-Auth-Key': cfg.getConfigValue('pdu', 'api_key')}, json=data)
-    print r.json()
-
-
-
-def main(argv):
-
-    iprange = None
+def scanner(ipaddr, uniq, interface, user, pw):
+    print 'Running FTP against: %s uniq: %s' % (ipaddr, uniq)
 
     try:
-        opts, args = getopt.gnu_getopt(argv,"i:",["iprange="])
-    except getopt.GetoptError:
-        print sys.argv[0]+' -i <iprange>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print sys.argv[0]+' -i <iprange>'
-            sys.exit()
-        elif opt in ("-i", "--iprange"):
-            iprange = arg
+        ftp = FTP(ipaddr+'%'+interface, user, pw)
+    except:
+        return False
 
-    if iprange is None:
-        print sys.argv[0]+' -i <iprange>'
-        sys.exit()
+    old_filename = 'config_old_'+uniq+'.ini'
+    new_filename = 'config_new_'+uniq+'.ini'
+    file = open(old_filename, 'wb')
+    print 'Retrieving config.ini from %s' % ipaddr
+    ftp.retrbinary('RETR config.ini', file.write)
+    ftp.quit()
 
-    scan_port = int(cfg.getConfigValue('pdu', 'scan_port'))
-    snmp_port = int(cfg.getConfigValue('pdu', 'snmp_port'))
-    ro_community = cfg.getConfigValue('pdu', 'ro_community')
-    rw_community = cfg.getConfigValue('pdu', 'rw_community')
-    dccode = cfg.getConfigValue('pdu', 'dccode')
-    url = '%s/pdu/update' % cfg.getConfigValue('pdu', 'api_base')
+    file.close()
 
-    nm = PortScanner()
-    nm.scan(hosts=iprange, arguments='-n -p %s' % (scan_port))
+    f1 = open(old_filename, 'r')
+    f2 = open(new_filename, 'w')
+    triggered = False
+    for line in f1:
+        if 'IPv4=disabled' in line:
+            triggered = True
+            f2.write(line.replace('disabled', 'enabled'))
+        else:
+            f2.write(line)
+    f1.close()
+    f2.close()
 
-    for host in nm.all_hosts():
-        state = nm[host]['tcp'][scan_port]['state']
-        t = Thread(target=probePdu, args=(host, state, snmp_port, ro_community, rw_community, dccode, url))
-        t.start()
+    if triggered is True:
+        try:
+            ftp = FTP(ipaddr+'%'+interface, user, pw)
+        except:
+            return False
+        print 'Storing config.ini to %s' % ipaddr
+        ftp.storbinary('STOR config.ini', open(new_filename, 'rb'))
+        ftp.quit()
 
-if __name__ == "__main__":
-   main(sys.argv[1:])
+
+content = None
+with open(file, 'r') as f:
+    content = f.read()
+
+lines = []
+if content is None:
+    exit()
+else:
+    lines = content.splitlines()
+
+for ipaddr in lines:
+    sh = sha1(ipaddr)
+    t = Thread(target=scanner, args=(ipaddr, sh.hexdigest(), interface, defaultUser, defaultPass))
+    t.start()
+    time.sleep(.5)
